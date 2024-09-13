17498f8917f2c16bee785f67d0c9e8f31a25bd72
Davi Chagas <davih.chagas108@gmail.com>
2024-09-13 12:35:09 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -1,9 +1,112 @@
+const { select, input, checkbox } = require('@inquirer/prompts');
+
 let meta = {
-    value: 'ler um livro por mês',
-    checked: false,
-    isChecked: () => {}
+    value: 'Tomar 3L de água por dia',
+    checked: false
 }
 
-console.log(meta.value)
+let metas = [ meta ]
 
-const criarMeta = () => {}
+const cadastrarMeta = async () => {
+    const meta = await input ({message: "Digite a meta:"});
+
+    if(meta.length == 0){
+        console.log("A meta não pode ser vazia");
+        return
+    }
+
+    metas.push({
+        value: meta, checked: false
+    })
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+        meta.checked = true
+    })
+
+    console.log("Meta(s) marcadas como concluída(s)")
+}
+
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log("Não existem metas realizadas!")
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+}
+
+const start = async () => {
+    while(true) {
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastra meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar meta",
+                    value: "listar"
+                },
+                {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                },
+                
+            ]
+
+        });
+        
+        switch(opcao) {
+            case "cadastrar":
+                await cadastrarMeta();
+                console.log(metas)
+                break;
+            
+            case "listar":
+                await listarMetas();
+                break;
+
+            case "realizadas":
+                await metasRealizadas();
+                break;
+
+            case "sair":
+                return
+        }
+    }
+}
+
+start();
